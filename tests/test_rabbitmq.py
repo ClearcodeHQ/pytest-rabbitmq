3140dfb37a2176e84e812cfc9622d5d6456d6fb1
Grzegorz Śliwiński <g.sliwinski@clearcode.cc>
2019-11-22 11:08:44 +0100

    linters

--- tests/test_rabbitmq.py
+++ tests/test_rabbitmq.py
@@ -75,5 +75,8 @@ def test_random_port(rabbitmq_rand):
 
 def test_random_port_node_names(rabbitmq_rand_proc2, rabbitmq_rand_proc3):
     """Test node names for different processes."""
+    # pylint:disable=protected-access
     assert (rabbitmq_rand_proc2._envvars['RABBITMQ_NODENAME'] !=
             rabbitmq_rand_proc3._envvars['RABBITMQ_NODENAME'])
+
+    # pylint:enable=protected-access
